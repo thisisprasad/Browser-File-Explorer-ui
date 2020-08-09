67a90d5745666cd98e4a630c21579133a34ac726
Prasad <psdudeman39@gmail.com>
2020-08-09 23:19:56 +0530

    Context Menu created after mouse right-click. Code is not clean

--- js/libs/views/XplorerItemContext/XplorerItemContextmenuView.js
+++ js/libs/views/XplorerItemContext/XplorerItemContextmenuView.js
@@ -9,6 +9,7 @@ define([
     var self;
     
     var ItemContextView = Backbone.View.extend({
+        
         initialize: function(options){
             self = this;
             self.options = options;
@@ -27,13 +28,59 @@ define([
                 html += self._generateOptionHtml(option);
             }
             self.$el.find("#item_context_menu_options").append(html);
+            self.$el.find("#item_context_menu_options").css("display", "block");
+        },
+        
+        openContextMenu: function(event){
+            var contextMenuActive = "context_menu--active";
+            var menu = document.querySelector("#item_context_menu_options");
+            menu.classList.add(contextMenuActive);
+            console.log("opening context menu...");
+            var clickCoords = self._getPosition(event);
+            var clickCoordsX = clickCoords.x;
+            var clickCoordsY = clickCoords.y;
+            
+            var menuWidth = menu.offsetWidth + 4;
+            var menuHeight = menu.offsetHeight + 4;
+            
+            var windowWidth = window.innerWidth;
+            var windowHeight = window.innerHeight;
+            
+            if((windowWidth - clickCoordsX) < menuWidth){
+                menu.style.left = (windowWidth - menuWidth) - 0 + "px";
+            } else {
+                menu.style.left = clickCoordsX-0 + "px";
+            }
+            
+            if(Math.abs(windowHeight - clickCoordsY) < menuHeight){
+                menu.style.top = (windowHeight - menuHeight)-0 + "px";
+            } else {
+                menu.style.top = clickCoordsY-0 + "px";
+            }
+        },
+        
+        _getPosition: function(e){
+            var posx = 0, posy = 0;
+            if(!e) var e = window.event;
+            if(e.pageX || e.pageY){
+                posx = e.pageX;
+                posy = e.pageY;
+            } else if(e.clientX || e.clientY){
+                posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            }
+            
+            return {
+                x: posx,
+                y: posy
+            };
         },
         
         _generateOptionHtml: function(option) {
             console.log("option: " + option + " html to be created");
             
             var html = "";
-            html += " <li data-option='"+option+"' >" + option + " </li> ";
+            html += " <li data-option='"+option+"' class='menu_option' >" + option + " </li> ";
             return html;
         }
     });
