3d6ac45a4f808b30e4ef1fbc10967d3b3629eef5
Prasad <psdudeman39@gmail.com>
2020-08-10 14:22:32 +0530

    contextmenu works. Documentation for app integration-configuration is written in a separate file.

---
+++ config/AppConfig.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function($, _, Backbone){
+    var AppConfig = {
+        name: "FileXplorer",
+        version: "1.0",
+        apps: [
+            {
+                name: "File Encryption",
+                contextmenu: {
+                    
+                }
+            },
+        ]
+    }
+});
